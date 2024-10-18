0d4d3df3256ca1e4bfedd55a29988d91850d2a80
Piotr Stradowski <stradowski.stradek@gmail.com>
2024-10-18 20:30:57 +0200

    Add level class for the game

---
+++ dev/src/game/level.cpp
@@ -0,0 +1,27 @@
+/*
+	Copyright (c) 2024 Piotr Stradowski. All rights reserved.
+	Software distributed under the permissive MIT License.
+*/
+
+#pragma once
+
+#include "level.h"
+
+Level::Level()
+{
+
+
+	LoadAssets();
+}
+
+Level::~Level()
+{
+}
+
+void Level::LoadAssets()
+{
+}
+
+void Level::DestroyAssets()
+{
+}
---
+++ dev/src/game/level.h
@@ -0,0 +1,14 @@
+/*
+	Copyright (c) 2024 Piotr Stradowski. All rights reserved.
+	Software distributed under the permissive MIT License.
+*/
+
+#pragma once
+
+class Level
+{
+	Level();
+	~Level();
+	void LoadAssets();
+	void DestroyAssets();
+};
